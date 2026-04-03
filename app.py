a4758fc17732c4569f79a72cd29386897c0fb946
MD <mounikadevimd2000@gmail.com>
2026-04-02 21:33:26 -0500

    Fixed cloud FAISS loading

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import streamlit as st
 from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 st.title("📄 Enterprise Knowledge Copilot")
 st.write("Ask questions from your PDF")
 
+# Load PDF
+loader = PyPDFLoader("data/sample.pdf")
+docs = loader.load()
+
+# Create embeddings
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-db = FAISS.load_local(
-    "vectorstore",
-    embeddings,
-    allow_dangerous_deserialization=True
-)
+# Create vector DB dynamically
+db = FAISS.from_documents(docs, embeddings)
 
 query = st.text_input("Ask your question")
 
